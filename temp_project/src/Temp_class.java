d25ba85750b734edc516a5cac225136c536e3ba4
abdulaziz <abdulazizLola@proton.me>
2023-07-20 12:37:14 +0300

    20/Jul/2023 class changes

--- temp_project/src/Temp_class.java
+++ temp_project/src/Temp_class.java
@@ -1,4 +1,3 @@
-import javax.print.attribute.standard.JobStateReason;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -14,6 +13,10 @@ public class Temp_class extends JFrame implements ActionListener {
 
     ButtonGroup radioGroup;
     JCheckBox checkBox;
+    JList list;
+    JComboBox dropDown;
+    JScrollPane scrollPane;
+    String[] cities = {"Makkah", "Jeddah", "Riydadh", "Abha"};
     @Override
     public void actionPerformed(ActionEvent actionEvent) {
         if(actionEvent.getSource() == button){
@@ -33,29 +36,49 @@ public class Temp_class extends JFrame implements ActionListener {
         }
     }
     Temp_class(){
-    this.setSize(500,500);
-    this.setDefaultCloseOperation(this.EXIT_ON_CLOSE);
-    this.setLocationRelativeTo(null);
-    textArea = new JTextArea(1,20);
-    button = new JButton("click!");
-    radioButton1 = new JRadioButton("hello", true);
-    radioButton2 = new JRadioButton("hi");
-    radioButton3 = new JRadioButton("hey");
-    checkBox = new JCheckBox("this is a check box");
-    radioGroup = new ButtonGroup();
-    radioGroup.add(radioButton1);
-    radioGroup.add(radioButton2);
-    radioGroup.add(radioButton3);
-    button.addActionListener(this::actionPerformed);
-    panel = new JPanel(new FlowLayout(FlowLayout.CENTER));
-    panel.add(textArea);
-    panel.add(button);
-    panel.add(radioButton1);
-    panel.add(radioButton2);
-    panel.add(radioButton3);
-    panel.add(checkBox);
-    this.add(panel);
-    this.setVisible(true);
+
+        this.setSize(500,500);
+        this.setDefaultCloseOperation(this.EXIT_ON_CLOSE);
+        this.setLocationRelativeTo(null);
+
+        textArea = new JTextArea(1,20);
+        textArea.setEditable(false);
+
+        button = new JButton("click!");
+        button.addActionListener(this::actionPerformed);
+        button.setOpaque(true);
+
+        radioButton1 = new JRadioButton("CS", true);
+        radioButton2 = new JRadioButton("OIT");
+        radioButton3 = new JRadioButton("OIS");
+        radioGroup = new ButtonGroup();
+        radioGroup.add(radioButton1);
+        radioGroup.add(radioButton2);
+        radioGroup.add(radioButton3);
+
+        checkBox = new JCheckBox("this is a check box");
+
+        list = new JList(cities);
+        dropDown = new JComboBox(cities);
+        scrollPane = new JScrollPane(list);
+
+        list.setSelectionMode(0);
+        list.setBorder(BorderFactory.createLineBorder(Color.BLACK, 1, true));
+        list.setVisibleRowCount(3);
+        dropDown.setBorder(BorderFactory.createLineBorder(Color.red, 2, false));
+
+        panel = new JPanel(new FlowLayout(FlowLayout.CENTER));
+        panel.add(textArea);
+        panel.add(button);
+        panel.add(radioButton1);
+        panel.add(radioButton2);
+        panel.add(radioButton3);
+        panel.add(checkBox);
+        panel.add(scrollPane);
+        panel.add(dropDown);
+        this.add(panel);
+
+        this.setVisible(true);
     }
 
 }
