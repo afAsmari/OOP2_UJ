ab95355aae12be1dd34248b5264675d4a16e9f0b
abdulaziz <abdulazizLola@proton.me>
2023-07-25 12:46:06 +0300

    class 25/Jul/2023

--- gui_app5/src/MainWindow.java
+++ gui_app5/src/MainWindow.java
@@ -3,20 +3,30 @@ import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 
 public class MainWindow extends JFrame {
     // declaration of universal objects and variables
     JPanel panel;
-    JLabel label;
+    JLabel label1;
+    JLabel label2;
+    JLabel label3;
+    JLabel label4;
     JList list;
     JComboBox comboBox;
     JSlider slider;
+    JTextArea textArea;
+    JScrollPane scrollPane;
+    JTextField textField;
+    JFileChooser fileChooser;
+    JButton button;
     String[] cities = {"jeddah", "makkah", "abha"};;
     MainWindow(){
         // main configurations
-        this.setSize(300, 300);
+        this.setSize(1000, 700);
         this.setLocationRelativeTo(null);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setTitle(".");
@@ -24,10 +34,26 @@ public class MainWindow extends JFrame {
         // initialization of components
         panel = new JPanel();
         list = new JList(cities);
-        label = new JLabel("choose a city");
+        label1 = new JLabel("choose a city");
+        label1.setForeground(Color.BLACK);
+        label2 = new JLabel("choose a city from the dropdown menu");
+        label2.setForeground(Color.RED);
+        label3 = new JLabel("move the slider");
+        label3.setForeground(Color.BLUE);
+        label4 = new JLabel();
+        label4.setForeground(Color.ORANGE);
+
         comboBox = new JComboBox<>(cities);
         slider = new JSlider(0, 100);
-
+        textArea = new JTextArea(5,5);
+        textArea.setLineWrap(true);
+        textArea.setWrapStyleWord(true);
+        scrollPane = new JScrollPane(textArea);
+        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+        textField = new JTextField(5);
+        fileChooser = new JFileChooser();
+        button = new JButton("select file");
+        button.addActionListener(new buttonClick());
         // slider configurations
         slider.setOrientation(JSlider.HORIZONTAL);
         slider.setMinorTickSpacing(5);
@@ -37,9 +63,15 @@ public class MainWindow extends JFrame {
 
         // adding components to the panel and frame
         panel.add(list);
-        panel.add(label);
+        panel.add(label1);
+        panel.add(label2);
+        panel.add(label3);
         panel.add(comboBox);
         panel.add(slider);
+        panel.add(scrollPane);
+        panel.add(textField);
+        panel.add(button);
+        panel.add(label4);
         this.add(panel);
 
         // adding event listeners
@@ -54,7 +86,7 @@ public class MainWindow extends JFrame {
 
         @Override
         public void valueChanged(ListSelectionEvent listSelectionEvent) {
-            label.setText(cities[list.getSelectedIndex()]);
+            label1.setText(cities[list.getSelectedIndex()]);
         }
     }
 
@@ -62,7 +94,7 @@ public class MainWindow extends JFrame {
 
         @Override
         public void actionPerformed(ActionEvent actionEvent) {
-            label.setText((String) comboBox.getSelectedItem());
+            label2.setText((String) comboBox.getSelectedItem());
         }
     }
 
@@ -70,7 +102,21 @@ public class MainWindow extends JFrame {
 
         @Override
         public void stateChanged(ChangeEvent changeEvent) {
-            label.setText(Integer.toString(slider.getValue()));
+            label3.setText(Integer.toString(slider.getValue()));
+        }
+    }
+
+    private class buttonClick implements ActionListener{
+
+        @Override
+        public void actionPerformed(ActionEvent actionEvent) {
+            int status = fileChooser.showOpenDialog(null);
+            if(status == JFileChooser.APPROVE_OPTION){
+                File selectedFile = fileChooser.getSelectedFile();
+                String filename = selectedFile.getPath();
+                fileChooser.cancelSelection();
+                label4.setText(filename);
+            }
         }
     }
 }
