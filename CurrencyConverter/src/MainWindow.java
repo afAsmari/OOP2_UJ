10d54977ae1426711f5b67c5cd8b2b0b7ffb3b8c
abdulaziz <abdulazizLola@proton.me>
2023-07-30 12:24:07 +0300

    class 30/Jul/2023

--- CurrencyConverter/src/MainWindow.java
+++ CurrencyConverter/src/MainWindow.java
@@ -2,7 +2,6 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
